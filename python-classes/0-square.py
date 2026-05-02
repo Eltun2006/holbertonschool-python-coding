3485c1bf23c497daa801f09a6d00d442bc89d046
Eltun2006 <eltun.memmedli.06@gmail.com>
2026-05-02 19:59:52 +0000

    Add Square class - python-classes task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Defines a square by its size."""
+
+    def __init__(self, size):
+        self.__size = size
